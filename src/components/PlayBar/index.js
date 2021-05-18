382bfc05a6a2d5f324342efb45bc3a7d2ba4c2ac
code1379 <coderxialuo@gmail.com>
2021-05-18 15:59:14 +0800

    写了一部分

--- src/components/PlayBar/index.js
+++ src/components/PlayBar/index.js
@@ -1,6 +1,82 @@
 import React, { memo } from 'react';
+import { useState } from 'react';
+import { getSizedImage } from '../../utils';
 
 import { PlayBarWrapper } from './style';
+import { Slider } from 'antd';
+import { useRef } from 'react';
+
 export default memo(function PlayBar() {
-  return <PlayBarWrapper>playbar</PlayBarWrapper>;
+  const [isShow, setIsShow] = useState(true);
+  const [playStat, setPlayStat] = useState(false);
+  const audioRef = useRef(null);
+
+  const handlePlay = () => {
+    if (!playStat) {
+      audioRef.current.play();
+      setPlayStat(true);
+    } else {
+      audioRef.current.pause();
+      setPlayStat(false);
+    }
+  };
+  return (
+    <PlayBarWrapper show={isShow}>
+      <div className='hand' onClick={() => setIsShow(!isShow)}></div>
+      <div className='bg'>
+        <div className='bg-l playbar-spirit'></div>
+        <div className='lock playbar-spirit'></div>
+        <div className='bg-r playbar-spirit'></div>
+      </div>
+
+      <div className='wrap-v2'>
+        <audio
+          ref={audioRef}
+          src='https://music.163.com/song/media/outer/url?id=571340039.mp3'
+        />
+        <div className='btns'>
+          <div className='prev btn playbar-spirit'></div>
+          <div
+            className={
+              playStat ? 'pause play btn playbar-spirit' : 'play btn playbar-spirit'
+            }
+            onClick={handlePlay}
+          ></div>
+          <div className='next btn playbar-spirit'></div>
+        </div>
+
+        <div className='play-info'>
+          <div className='img-container'>
+            <img
+              src={getSizedImage(
+                'https://p2.music.126.net/aoxhrGqswLmyPDib4rckGQ==/109951165959603807.jpg',
+                34
+              )}
+              alt=''
+            />
+            <div className='mask playbar-spirit'></div>
+          </div>
+          <div className='content'>
+            <div className='head'>
+              <span>抢先</span>
+              <span>Todd Li</span>
+            </div>
+            <div className='slide-bar'>
+              <Slider defaultValue={30} />
+              <div className='time'> 00:36/02:22</div>
+            </div>
+          </div>
+        </div>
+        <div className='operator'>
+          <span>收藏</span>
+          <span>转发</span>
+        </div>
+        <div className='control'>
+          <div className='volume'></div>
+          <div className='pattern'>单曲循环</div>
+          <div className='playList'></div>
+        </div>
+      </div>
+    </PlayBarWrapper>
+  );
 });
--- src/components/PlayBar/style.js
+++ src/components/PlayBar/style.js
@@ -1,7 +1,138 @@
-import styled from 'styled-components'
+import styled from 'styled-components';
+
+import playBtn from '@/assets/images/ply_btn.png';
+import statBar from '@/assets/images/statbar.png';
 
 export const PlayBarWrapper = styled.div`
-  height: 47px;
   position: fixed;
-  bottom: 0;
-`
+  z-index: 20;
+  transition: all 0.3s ease;
+  height: 53px;
+  left: 0;
+  right: 0;
+  width: 100%;
+  bottom: ${(props) => (props.show ? 0 : '-50px')};
+  .hand {
+    height: 20px;
+    width: 100%;
+    cursor: pointer;
+    position: absolute;
+    z-index: 999;
+    top: -10px;
+  }
+  .bg {
+    display: flex;
+    .bg-l {
+      flex: 1;
+      height: 53px;
+      background-position: 0 0;
+      background-repeat: repeat-x;
+    }
+    .lock {
+      width: 52px;
+      height: 67px;
+      background-position: 0 -380px;
+      position: relative;
+      top: -14px;
+    }
+    .bg-r {
+      width: 10px;
+      height: 53px;
+      background-position: 0 0;
+      background-repeat: repeat-x;
+    }
+  }
+
+  .wrap-v2 {
+    position: absolute;
+    left: 50%;
+    margin: 0 auto;
+    top: 6px;
+    height: 47px;
+    transform: translateX(-50%);
+    display: flex;
+
+    .btns {
+      display: flex;
+      align-items: center;
+      .btn {
+        cursor: pointer;
+        width: 28px;
+        height: 28px;
+        &.next {
+          background-position: -80px -130px;
+        }
+        &.prev {
+          background-position: 0 -130px;
+        }
+      }
+
+      .play {
+        margin: 0 10px;
+        width: 36px;
+        height: 36px;
+        background-position: 0 -204px;
+        &.pause {
+          background-position: 0 -165px;
+        }
+      }
+    }
+
+    .play-info {
+      margin-left: 30px;
+      display: flex;
+      align-items: center;
+
+      .img-container {
+        position: relative;
+        width: 34px;
+        height: 34px;
+        .mask {
+          /* position: absolute; */
+          /* left: 0; */
+          /* top: 0; */
+          width: 34px;
+          height: 35px;
+          background-position: 0 -80px;
+        }
+      }
+      .content {
+        margin-left: 15px;
+        height: 100%;
+        padding-top: 6px;
+        .head {
+          color: #fff;
+          margin-bottom: 6px;
+        }
+        .slide-bar {
+          display: flex;
+          .ant-slider {
+            margin: 0;
+            width: 493px;
+            .ant-slider-rail {
+              height: 9px;
+              border-radius: 4.5px;
+              background: url(${statBar}) no-repeat;
+              background-position: right 0;
+            }
+            .ant-slider-track {
+              height: 9px;
+              background: url(${statBar}) no-repeat;
+              background-position: left -66px;
+            }
+            .ant-slider-handle {
+              margin-top: -8px;
+              width: 22px;
+              height: 24px;
+              background: url(${playBtn}) no-repeat;
+              border: none;
+            }
+          }
+          .time {
+            color: #fff;
+          }
+        }
+      }
+    }
+  }
+`;
